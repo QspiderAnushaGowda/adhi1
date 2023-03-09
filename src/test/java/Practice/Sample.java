185b0d7b15cbae15b44474d177a02d76dc548136
email <anunswamy@gmail.com>
2023-03-09 21:47:16 +0530

    first commmit

---
+++ src/test/java/Practice/Sample.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+	@Test
+	public void t1()
+	{
+		System.out.println("selenium");
+		
+		System.out.println("manual ");
+	}
+
+}
